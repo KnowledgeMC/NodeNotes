5d1544405f39f756ed5ca6cd564eafca9566fcc9
Roy Hwang <tupaclypse@gmail.com>
2017-02-17 19:13:44 -0800

    Import index.js file and synchronize models to database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//Import index.js file from models directory
+const Models = require('./lib/models/');
+
 const Routes = require('./lib/routes');
 
 const Hapi = require('hapi');
@@ -13,9 +16,15 @@ server.connection({ port: Settings.port });
 
 server.route(Routes);
 
-server.start((err) => {
-  Hoek.assert(!err, err);
-
-  console.log('Server running at: ${server.info.uri}');
+// server.start((err) => {
+//   Hoek.assert(!err, err);
+//   console.log(`Server running at: ${server.info.uri}`);
+// });
+//Replacing above with:
+Models.sequelize.sync().then(() => {
+  server.start((err) => {
+    Hoek.assert(!err, err);
+    console.log(`Server running at: ${server.info.uri}`);
+  });
 });
-
+// Above code will synchronize the models to our database and when that's done, start the server
